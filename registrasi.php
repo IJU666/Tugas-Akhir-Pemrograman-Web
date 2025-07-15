384603b72ca74511a0c858f41a51af0ca234a0d4
IJU666 <fauzimaulanabandung2020@gmail.com>
2025-07-15 22:49:52 +0700

    ujiw

--- registrasi.php
+++ registrasi.php
@@ -21,9 +21,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Halaman Registrasi</h1>
+    <h1 class="judul">Halaman Registrasi</h1>
     <a href="Login.php">Login Akun</a>
 
     <form action="" method="post">
