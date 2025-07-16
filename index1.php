26fc4a40f03e76f4db5a888db46ae1fa1cf0e739
IJU666 <fauzimaulanabandung2020@gmail.com>
2025-07-16 23:41:01 +0700

    ngerjain yang si itu tuh

--- index1.php
+++ index1.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION["login"])) {
   header("Location: login.php");
   exit;
 }
-$totalDataPerPage = 3;
+$totalDataPerPage = 5;
 // $result = query("SELECT * FROM dosen ORDER BY id ASC LIMIT 0,3");
 // $result = query("SELECT * FROM dosen ORDER BY id ASC LIMIT 0,$totalDataPerPage");
 $totalDataTabel = count(query("SELECT * FROM dosen "));
@@ -32,66 +32,83 @@ if (isset($_POST["cari"])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-    <div style="margin: auto;">
-      <a href="logout.php">Logout</a>
-  <h1 style="text-align:center;">Daftar Dosen</h1>
-  <form action="" method="post">
-    <input type="text" name="keyword" id="" size="50" placeholder="Masukan Kata Kunci" autocomplete="off">
-    <button type="submit" name="cari">Cari</button>
-  </form>
-  <a href="tambah.php" style="text-align:right;">Tambah data dosen</a>
-  <br>
-  <!-- <?php for ($i=1; $i <= $totalPage; $i++) : ?>
-      <a href="index1.php?page=<?= $i; ?>"><?= $i; ?></a>
-    <?php endfor; ?> -->
-  <?php if ($activePage > 1) : ?>
-      <a href="?page= <?= $activePage - 1; ?>">&laquo </a>
-    <?php endif ?>
-    
-    <?php for ($i=1; $i <= $totalPage; $i++) : ?>
-      <?php if ($i == $activePage) : ?>
-        <a href="index1.php?page=<?= $i; ?>" style="font-weight: bold; color: blue;"><?= $i; ?></a>
-      <?php else :?>
-      <a href="index1.php?page=<?= $i; ?>"><?= $i; ?></a>
-      <?php endif;?>
-    <?php endfor; ?>
 
-  
+      
+  <h1 style="text-align:center;">Daftar Dosen</h1>
+  <div class="table-position">
     
-    <?php if ($activePage < $totalPage) : ?>
-      <a href="?page= <?= $activePage + 1; ?>">&raquo </a>
-    <?php endif ?>
+  <form action="" method="post">
+  <div class="search-position">
+    <div class="form-position">
+      <div class="form-search">
+      <input type="text" name="keyword" id="" size="50" placeholder="Masukan Kata Kunci" class="form-custom" autocomplete="off">
+      <button type="submit" name="cari" class="regist-button">Cari</button>
+      </div>
+      <a href="tambah.php" class="regist-button">Tambah data dosen</a>
+  </div>
+  </form>
   <table border="1" cellpadding="10" cellspacing="0" >
     <tr>
       <th>No.</th>
-      <th>Aksi</th>
       <th>Foto</th>
       <th>Nama</th>
       <th>NIDN</th>
       <th>Email</th>
       <th>Prodi</th>
+      <th>Aksi</th>
     </tr>
         <?php foreach ($result as $row) : ?>
       <tr>
         <td><?=$row["id"];?></td>
-          <td>
-              <a href="ubah.php?id= <?= $row['id'];?>">ubah</a> |
-              <a href="delete.php?id= <?= $row['id'];?>" onclick="return confirm('yakin menghapus data?')">hapus</a>
-            </td>
         <td><img src="img/<?= $row["foto"]; ?>" width="50"></td>
         <td><?= $row["nama"]; ?></td>
         <td><?= $row["NIDN"]; ?></td>
         <td><?= $row["email"]; ?></td>
         <td><?= $row["prodi"]; ?></td>
+        <td>
+          <div style="display: flex; gap: 5px;">
+            <a href="ubah.php?id= <?= $row['id'];?>" class="ubah">ubah</a> |
+            <a href="delete.php?id= <?= $row['id'];?>" onclick="return confirm('yakin menghapus data?')" class="hapus">hapus</a>
+            </div>
+          </td>
       </tr>
     <?php endforeach; ?>
 
   </table>
+  <div class="pagination">
+     <ul class="custom-pagination">
+    <li>
+      <?php if ($activePage > 1) : ?>
+      <a href="?page= <?= $activePage - 1; ?>" aria-label="Previous">&laquo; </a>
+      <?php endif ?>
+    </li>
+    <li>
+        <?php for ($i=1; $i <= $totalPage; $i++) : ?>
+        <?php if ($i == $activePage) : ?>
+          <a href="index1.php?page=<?= $i; ?>" style="background-color: #368098; color: white; pointer-events: none;"><?= $i; ?></a>
+          <?php else :?>
+            <a href="index1.php?page=<?= $i; ?>"><?= $i; ?></a>
+            <?php endif;?>
+            <?php endfor; ?>
+    </li>
+    <li>
+      <?php if ($activePage < $totalPage) : ?>
+              <a href="?page= <?= $activePage + 1; ?>"aria-label="Next">&raquo;</a>
+              <?php endif ?>
+    </li>
+  </ul>  
   </div>
-
+              <div class="logout-position">
+              <a class="button-logout" href="logout.php">Logout</a>
+              </div>
+            </div>
 </body>
 </html>
