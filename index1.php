df849384a648e1f002f4bd99e24fa08c06809df9
IJU666 <fauzimaulanabandung2020@gmail.com>
2025-07-16 23:48:51 +0700

    debugging

--- index1.php
+++ index1.php
@@ -57,13 +57,13 @@ if (isset($_POST["cari"])) {
   </form>
   <table border="1" cellpadding="10" cellspacing="0" >
     <tr>
-      <th>No.</th>
-      <th>Foto</th>
-      <th>Nama</th>
-      <th>NIDN</th>
-      <th>Email</th>
-      <th>Prodi</th>
-      <th>Aksi</th>
+      <th class="table-heading">No.</th>
+      <th class="table-heading">Foto</th>
+      <th class="table-heading">Nama</th>
+      <th class="table-heading">NIDN</th>
+      <th class="table-heading">Email</th>
+      <th class="table-heading">Prodi</th>
+      <th class="table-heading">Aksi</th>
     </tr>
         <?php foreach ($result as $row) : ?>
       <tr>
@@ -74,7 +74,7 @@ if (isset($_POST["cari"])) {
         <td><?= $row["email"]; ?></td>
         <td><?= $row["prodi"]; ?></td>
         <td>
-          <div style="display: flex; gap: 5px;">
+          <div style="display: flex; gap: 5px; justify-content: center; align-items: center;">
             <a href="ubah.php?id= <?= $row['id'];?>" class="ubah">ubah</a> |
             <a href="delete.php?id= <?= $row['id'];?>" onclick="return confirm('yakin menghapus data?')" class="hapus">hapus</a>
             </div>
